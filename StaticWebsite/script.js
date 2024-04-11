f4e03b7d3caf503a64564fc79b0fd14ccc9db3d4
JohnLopez719 <30753364+johnlopez719@users.noreply.github.com>
2024-04-11 18:38:36 -0400

    Add files via upload

--- StaticWebsite/script.js
+++ StaticWebsite/script.js
@@ -1,6 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const themeToggleButton = document.getElementById('themeToggle');
-    themeToggleButton.addEventListener('click', function() {
-        document.body.classList.toggle('dark-theme');
-    });
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
 });
